73a8fcd1a0f6cd53f2527c29c49eef1c9d39bc1e
Stone Comstock <comstockstone@yahoo.com>
2022-04-13 20:23:43 -0700

    changing projects layout to card carousel

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ const navToggle = document.querySelector('.nav-toggle'); //create variable for n
 
 const navLinks = document.querySelectorAll('.nav__link'); //variable for all links in navbar
 
-
 //event listener for navToggle button
 navToggle.addEventListener('click', () => {
-    //add nav-open class to body, use to show nav menu and rotate nav toggle button in css
-    document.body.classList.toggle('nav-open');
+  //add nav-open class to body, use to show nav menu and rotate nav toggle button in css
+  document.body.classList.toggle('nav-open');
 });
 
 //add event listener for each nav link
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        //remove nav-open class to hide nav menu
-        document.body.classList.remove('nav-open');
-    })
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    //remove nav-open class to hide nav menu
+    document.body.classList.remove('nav-open');
+  });
 });
+
